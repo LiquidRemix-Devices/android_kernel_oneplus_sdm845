97219ecd16244c8898bf742b94df1c01fc6cd112
matthewdalex <matthewalex@outlook.com>
2018-06-01 15:29:53 -0400

    build: R8

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ export CROSS_COMPILE_ARM32=${HOME}/android/arm-linux-androideabi-4.9/bin/arm-lin
 DEFCONFIG="chewie_defconfig"
 
 # Kernel Details
-VER=".R7"
+VER=".R8"
 
 # Paths
 KERNEL_DIR=`pwd`
