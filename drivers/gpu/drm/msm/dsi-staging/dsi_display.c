d52ee95738b5a3b9bd7a806ce9496e7b5847740b
Erik Müller <pappschlumpf>
2018-06-12 22:16:24 +0200

    Dynamic Fsync 2.1 / Async Fsync

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -37,6 +37,10 @@
 #include <linux/notifier.h>
 #include <linux/sched.h>
 #include "../sde/sde_trace.h"
+#include <linux/pm_qos.h>
+#include <linux/cpufreq.h>
+#include <linux/pm_wakeup.h>
+#include <linux/lcd_notify.h>
 
 int backlight_min = 0;
 module_param(backlight_min, int, 0644);
@@ -7722,3 +7726,85 @@ MODULE_PARM_DESC(dsi_display1,
 	"msm_drm.dsi_display1=<display node>:<configX> where <display node> is 'secondary dsi display node name' and <configX> where x represents index in the topology list");
 module_init(dsi_display_register);
 module_exit(dsi_display_unregister);
+
+static int msm_drm_buffer_state_change(struct notifier_block *nb,
+        unsigned long val, void *data)
+{
+        int blank;
+        struct msm_drm_notifier *evdata = data;
+
+        if (!evdata || (evdata->id != 0))
+                return 0;
+
+        blank = *(int *)evdata->data;
+
+        switch (blank) {
+        case MSM_DRM_BLANK_POWERDOWN:
+                if (val == MSM_DRM_EARLY_EVENT_BLANK) {
+                        pm_qos_update_request(&lcdspeedup_little_cpu_qos,
+                                MIN_CPUFREQ);
+                        pm_qos_update_request(&lcdspeedup_big_cpu_qos,
+                                MIN_CPUFREQ);
+                        /* add print actvie ws */
+                        pm_print_active_wakeup_sources_queue(true);
+                        pr_debug("::: LCD start off :::\n");
+                }
+		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
+                break;
+        case MSM_DRM_BLANK_UNBLANK:
+                if (val == MSM_DRM_EARLY_EVENT_BLANK) {
+                        struct cpufreq_policy *policy;
+                         /* Speed up LCD on */
+                        /* Fetch little cpu policy
+                        * and drive the CPU towards target frequency
+                        */
+                        pm_qos_update_request_timeout(
+                                &lcdspeedup_little_cpu_qos, MAX_CPUFREQ,
+                                LCD_QOS_TIMEOUT);
+
+                        /* Fetch big cpu policy
+                        * and drive big cpu towards target frequency
+                        */
+                        policy = cpufreq_cpu_get(BIG_CPU_NUMBER);
+                        if (policy)  {
+                                cpufreq_driver_target(policy,
+                                        LCDSPEEDUP_BIG_CPU_QOS_FREQ,
+                                        CPUFREQ_RELATION_H);
+                                pm_qos_update_request_timeout(
+                                        &lcdspeedup_big_cpu_qos,
+                                        (MAX_CPUFREQ-4), LCD_QOS_TIMEOUT);
+                        } else
+                                return NOTIFY_OK;
+                        cpufreq_cpu_put(policy);
+                }
+
+                if (val == MSM_DRM_EVENT_BLANK) {
+                        sched_set_boost(NO_BOOST);
+                        /* remove print actvie ws */
+                        pm_print_active_wakeup_sources_queue(false);
+                        pr_debug("::: LCD is on :::\n");
+                }
+		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
+                break;
+        default:
+                break;
+        }
+        return NOTIFY_OK;
+}
+
+static struct notifier_block msm_drm_notifier_block = {
+        .notifier_call = msm_drm_buffer_state_change,
+        .priority = 1,
+};
+
+static int __init lcdscreen_speedup_init_pm_qos(void)
+{
+        msm_drm_register_client(&msm_drm_notifier_block);
+        pm_qos_add_request(&lcdspeedup_little_cpu_qos,
+                PM_QOS_C0_CPUFREQ_MIN, MIN_CPUFREQ);
+        pm_qos_add_request(&lcdspeedup_big_cpu_qos,
+                PM_QOS_C1_CPUFREQ_MIN, MIN_CPUFREQ);
+
+        return 0;
+}
+late_initcall(lcdscreen_speedup_init_pm_qos);
