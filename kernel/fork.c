ee4e399df93bb08cdb4e0edaf85ab18c7b48d1b8
Erik Müller <erik.lars.mueller@googlemail.com>
2018-08-11 02:15:42 +0200

    cpu_input_boost: Introduce cluster boost

--- kernel/fork.c
+++ kernel/fork.c
@@ -1955,8 +1955,8 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 	/* Boost CPU to the max for 1250 ms when userspace launches an app */
-	if (is_zygote_pid(current->pid))
-		cpu_input_boost_kick_max(1250);
+	//if (is_zygote_pid(current->pid))
+	//	cpu_input_boost_kick_max(1250);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
