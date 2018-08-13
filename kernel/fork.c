b01cdaea89263325d195b5a2dcce931644ddbbb1
Erik Müller <erik.lars.mueller@googlemail.com>
2018-08-13 18:39:02 +0200

    kernel: remove leftovers

--- kernel/fork.c
+++ kernel/fork.c
@@ -1953,16 +1953,6 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost CPU to the max for 1250 ms when userspace launches an app */
-<<<<<<< HEAD
-	//if (is_zygote_pid(current->pid))
-	//	cpu_input_boost_kick_max(1250);
-=======
-	if (is_zygote_pid(current->pid)) {
-		cpu_input_boost_kick_max(1250);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1250);
-	}
->>>>>>> 3288d93... kernel: Boost cpubw devfreq device to the max for app launches
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
