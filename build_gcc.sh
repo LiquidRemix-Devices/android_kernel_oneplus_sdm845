0286ffa8869866f368d01d48f4401d788e7c4f05
Erik Müller <erik.lars.mueller@googlemail.com>
2018-10-24 21:33:05 +0200

    Update to version 2.0.13

--- build_gcc.sh
+++ build_gcc.sh
@@ -21,7 +21,7 @@ export CROSS_COMPILE_ARM32=${HOME}/android/Toolchains/arm-linux-androideabi-8.2.
 DEFCONFIG="smurf_defconfig"
 
 # Kernel Details
-VER=".2.0.12.linaro"
+VER=".2.0.13.linaro"
 
 # Paths
 KERNEL_DIR=`pwd`
