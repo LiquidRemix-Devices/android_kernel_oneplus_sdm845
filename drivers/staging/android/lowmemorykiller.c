cf79adade93c2e9f5a0be1b563c8ee47086d47be
Erik Müller <erik.lars.mueller@googlemail.com>
2018-12-14 21:49:34 +0100

    lmk: switch boost to boost flex

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -1060,8 +1060,8 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 selftest_bypass:
 	selected_oom_score_adj = min_score_adj;
 
-	cpu_input_boost_kick_general(100);
-	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
+	cpu_input_boost_kick_flex();
+	devfreq_boost_kick_flex(DEVFREQ_MSM_CPUBW);
 
 	rcu_read_lock();
 
