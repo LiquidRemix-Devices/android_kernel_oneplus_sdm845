30a7cce2f590285814515d2b0e7fb4acc137abdf
Erik Müller <erik.lars.mueller@googlemail.com>
2019-01-06 17:11:30 +0100

    drm: finalize exposure adjustment for now

--- drivers/gpu/drm/drm_sysfs.c
+++ drivers/gpu/drm/drm_sysfs.c
@@ -439,6 +439,12 @@ static ssize_t SRGB_store(struct device *dev,
 	if (ret) {
 		pr_err("set sRGB mode(%d) fail\n", srgb_mode);
 	}
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+	if (srgb_mode == 0) 
+		ea_panel_set(0);
+	else
+		ea_panel_set(1);
+#endif
 	return count;
 }
 
@@ -475,6 +481,12 @@ static ssize_t DCI_P3_store(struct device *dev,
 	if (ret) {
 		pr_err("set dci-p3 mode(%d) fail\n", dci_p3_mode);
 	}
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+	if (dci_p3_mode == 0) 
+		ea_panel_set(0);
+	else
+		ea_panel_set(1);
+#endif
 	return count;
 }
 
@@ -486,10 +498,6 @@ static ssize_t night_mode_show(struct device *dev,
 	int night_mode = 0;
 
 	night_mode = dsi_display_get_night_mode(connector);
-	if (night_mode == 0) 
-		ea_panel_set(0);
-	else
-		ea_panel_set(1);
 
 	ret = scnprintf(buf, PAGE_SIZE, "night mode = %d\n"
 											"0--night mode Off\n"
@@ -515,6 +523,12 @@ static ssize_t night_mode_store(struct device *dev,
 	if (ret) {
 		pr_err("set night mode(%d) fail\n", night_mode);
 	}
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+	if (night_mode == 0) 
+		ea_panel_set(1);
+	else
+		ea_panel_set(0);
+#endif
 	return count;
 }
 
@@ -551,6 +565,12 @@ static ssize_t oneplus_mode_store(struct device *dev,
 	if (ret) {
 		pr_err("set oneplus mode(%d) fail\n", oneplus_mode);
 	}
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+	if (oneplus_mode == 0) 
+		ea_panel_set(1);
+	else
+		ea_panel_set(0);
+#endif
 	return count;
 }
 
@@ -587,6 +607,12 @@ static ssize_t adaption_mode_store(struct device *dev,
 	if (ret) {
 		pr_err("set adaption mode(%d) fail\n", adaption_mode);
 	}
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+	if (adaption_mode == 0) 
+		ea_panel_set(1);
+	else
+		ea_panel_set(0);
+#endif
 	return count;
 }
 
