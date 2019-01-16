41686c3d5e4134e203f7cc2ceb3bd17180679521
Erik Müller <erik.lars.mueller@googlemail.com>
2019-01-16 18:46:42 +0100

    Update config

--- build_gcc8.sh
+++ build_gcc8.sh
@@ -21,7 +21,7 @@ export CROSS_COMPILE_ARM32=${HOME}/android/Toolchains/arm-linux-androideabi-8.2.
 DEFCONFIG="smurf_defconfig"
 
 # Kernel Details
-VER=".2.0.53rc4.OOS"
+VER=".2.0.53xXx.OOS"
 
 # Paths
 KERNEL_DIR=`pwd`
